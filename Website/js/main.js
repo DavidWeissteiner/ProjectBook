165cedf01aa79a71a247e7f30a24c011442ce08b
David Weissteiner <david.weissteiner@gmail.com>
2017-11-13 15:49:02 +0100

    Siehe Protocol

---
+++ Website/js/main.js
@@ -0,0 +1,13 @@
+
+
+    $('.menuButton').click(function() {
+        $('.menu').animate({
+          left: "0px"
+        }, 200);
+
+        $('body').animate({
+          left: "250px"
+        }, 200);
+      });
+
+//https://www.codecademy.com/courses/web-beginner-en-zmn0b/0/1?content_from=make-an-interactive-website%3Ayour-first-program
