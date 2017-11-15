8ad13022fded91e9b2c241d93f8ee32ad8a826fd
David Weissteiner <david.weissteiner@gmail.com>
2017-11-15 13:03:35 +0100

    Zwischenspeicherung: ANATOMY

---
+++ Website/js/anatomy.js
@@ -0,0 +1,9 @@
+
+
+function headOnClick(){
+    if(document.getElementById("anatomyHead").src.includes("head.png")){
+        document.getElementById("anatomyHead").src="images/humanBody/headCard.png";
+    } else if(document.getElementById("anatomyHead").src.includes("headCard.png")){
+        document.getElementById("anatomyHead").src="images/humanBody/head.png";
+    }
+}
